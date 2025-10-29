1a9b28200dbed08b7fd18e1144d8933462ffd243
user-rdf <radif.mafazi@cakrawala.ac.id>
2025-10-29 11:10:23 +0700

    challenge

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -3,13 +3,15 @@ numbers = [10, 25, 7, 40, 15]
 print("List awal:", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
-print("Angka pertama:")
-print("Angka terakhir:")
+print("Angka pertama:", numbers[0])
+print("Angka terakhir:" ,numbers[4])
 
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
+numbers.append(60)
 print("List setelah ditambah:", numbers)
 
 # 4. TODO: Urutkan list secara descending
+reverse_list=numbers[::-1] #atau bisa menggunakan numbers.reverse()
 
 # 5. Tampilkan hasil akhir
-print("List urut descending:", numbers)
+print("List urut descending:", reverse_list)
--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,14 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
-
+while True:  
+    umur = int(input("Masukkan umur pengguna: "))
 # 2. TODO Tentukan kategori berdasarkan usia
+    if umur < 12:
+        print("Anak-anak")
+    elif 12 < umur < 17:
+        print("Remaja")
+    elif 18 < umur < 59:
+        print("Dewasa")
+    elif umur >= 60:
+        print("Lansia")
